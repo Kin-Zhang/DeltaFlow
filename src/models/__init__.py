88f49bca4b347b3e4bfd8b6ec0c0312d81fa9229
Qingwen Zhang <35365764+Kin-Zhang@users.noreply.github.com>
2025-11-21 11:50:07 +0000

    Merge ICP-Flow into codebase (#15)
    * cuda(histlib): cuda library from icp-flow project.
    
    * docs: update README with icp-flow in the official implementation.
    * conf(optimization-based): update all config files.
    * todo: update model file, double check with yancong and Qingwen confirm that: icp-flow results can be reproduced and tested.
    
    * docs: fix small typo, and starting updating model file.
    
    * feat(icp): core icp files, tested successfully.
    
    * hotfix(runner): Refactor metrics accumulation to use speed buckets
    
    * Improve ICPFlow import error message
    
    * docs: move to reproduce
    As the official review didn't finish, will open another request once Yancong have time to review/improve.
    
    * fix(runner): val mode add valid to align waymo validation run.
    * update master port also for multi-program run.
    
    * revert: check the range_bucket is range of speed/distance.
    revert to previous fix. successfully run on all opt method.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -43,3 +43,11 @@ try:
     from .fastnsf import FastNSF
 except ImportError:
     print("--- WARNING [model]: FastNSF is not imported, as it requires FastGeodis lib which is not installed.")
+
+# following need install extra package:
+# * pip install pytorch3d assets/cuda/histlib
+try:
+    from .icpflow import ICPFlow
+except ImportError as e:
+    print("--- WARNING [model]: ICPFlow is not imported, as it requires pytorch3d lib which is not installed.")
+    print(f"Detail error message\033[0m: {e}. Just ignore this warning if code runs without these models.")
